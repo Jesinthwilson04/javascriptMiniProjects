9b89b4d242814e0fede6909540b208043d5fec7e
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-12-06 15:46:52 +0530

    js

--- primeChecker/index.js
+++ primeChecker/index.js
@@ -1,1 +1,17 @@
-const numberInput=document.getElementById('number-input');
+const number=document.getElementById("number");
+const checkbtn=document.getElementById("checkPrime");
+const display=document.getElementById("display")
+
+checkbtn.addEventListener("click",function(){
+  let value=Number(number.value);
+  for( i=2;i<=Math.sqrt(value);i++)
+    if(value%i==0)
+      {
+        display.innerText="Not a Prime"
+}
+  else{
+    display.innerText="Prime"
+  }
+
+})
+
